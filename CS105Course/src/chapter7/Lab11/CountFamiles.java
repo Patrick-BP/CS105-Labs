be89512034b30e614e4fecfd54e2ad791e6c0bba
bp <bp@localhost>
2021-09-20 10:17:13 -0500

    editing countfamiles

--- CS105Course/src/chapter7/Lab11/CountFamiles.java
+++ CS105Course/src/chapter7/Lab11/CountFamiles.java
@@ -7,37 +7,52 @@ public class CountFamiles {
 		Scanner key = new Scanner(System.in);
 		System.out.println("Enter number of families");
 		int famNum = key.nextInt();
-		
 		double[] famIncomes = new double[famNum];
 		
-		for(int i = 0; i< famIncomes.length; i++) {
-			System.out.println("Enter income for : " + i);
-			famIncomes[i] = key.nextDouble();
-		}
 		
+		PopulateArray(famIncomes);
+		
+		showArray(famIncomes);
+		
+		findLowIncomes(famIncomes);		
+		
+	}
+	
+	
+	public static void PopulateArray(double[] x) {
+		Scanner key = new Scanner(System.in);
+		for(int i = 0; i< x.length; i++) {
+			System.out.println("Enter income for : " + i);
+			x[i] = key.nextDouble();
+		}
+	}
+	
+	public static void showArray(double[] x) {
 		System.out.print("[ ");
-		for(int i = 0; i<famIncomes.length; i++) {
+		for(int i = 0; i< x.length; i++) {
 			
-			System.out.print(famIncomes[i] + " " );
+			System.out.print( x[i] + " " );
 			
 		}
 		System.out.println(" ]");
-		double max = famIncomes[0];
-		
-		
-		for(int i = 0; i < famIncomes.length; i++) {
-			if(famIncomes[i] > max) max = famIncomes[i];
-		}
-		
-		System.out.println("maximum income is : " +max);
+	}
+	
+	
+	
+	public static void findLowIncomes(double[] x) {
+		double max = x[0];
 		int count = 0;
 		double maxtenpercent = max * 0.1;
 		System.out.println("10% of maximum income is : " + maxtenpercent);
 				
-		for(int i = 0; i < famIncomes.length; i++) {
-			if(famIncomes[i] < maxtenpercent) count++;
+		for(int i = 0; i < x.length; i++) {
+			if(x[i] < maxtenpercent) count++;
 		}
 		System.out.println(count + " family member is making less than 10% of $" + max);
-		key.close();
+		for(int i = 0; i < x.length; i++) {
+			if(x[i] > max) max = x[i];
+		}
+
 	}
+
 }
